d32845e80515240d63ff9018bc9b382afa6bf551
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-03 18:15:27 +0330

    configure db context

---
+++ NZWalks.API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static IServiceCollection AddDbContextServices(this IServiceCollection services , IConfiguration configuration)
+    {
+        services.AddDbContext<NZWalksDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("NZWalksConnectionString"));
+        });
+        
+        return services;
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -2,10 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+builder.Services.AddDbContextServices(builder.Configuration);
 
 var app = builder.Build();
 
