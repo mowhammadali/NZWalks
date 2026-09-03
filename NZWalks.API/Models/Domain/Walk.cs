8ded0e923cef7799e2ef1e736d8741e4a2fdd40d
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-03 17:41:53 +0330

    create domain models

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.Domain;
+
+public class Walk
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double LengthInKm { get; set; }
+    public string WalkImageUrl { get; set; }
+    [ForeignKey("Difficulty")] public Guid DifficultyId { get; set; }
+    public Difficulty Difficulty { get; set; }
+    [ForeignKey("Region")] public Guid RegionId { get; set; }
+    public Region Region { get; set; }
+}
