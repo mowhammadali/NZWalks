7a1ca45adecdbeb6abe1cadadd622499a250ab5c
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-05 18:50:07 +0330

    add region delete restrict migration

---
+++ NZWalks.API/Migrations/20260905151728_Restrict_Delete_Region.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Restrict_Delete_Region : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_Regions_RegionId",
+                table: "Walks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_Regions_RegionId",
+                table: "Walks",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_Regions_RegionId",
+                table: "Walks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_Regions_RegionId",
+                table: "Walks",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
