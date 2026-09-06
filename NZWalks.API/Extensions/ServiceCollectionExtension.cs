2139ab980a036a9a2314320e61bbe5fdc7f08aba
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-06 12:38:36 +0330

    setup adding authentication

--- NZWalks.API/Extensions/ServiceCollectionExtension.cs
+++ NZWalks.API/Extensions/ServiceCollectionExtension.cs
@@ -1,5 +1,8 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NZWalks.API.Data;
 using NZWalks.API.Mapping;
 using NZWalks.API.Repositories;
@@ -39,6 +42,29 @@ public static class ServiceCollectionExtension
     public static IServiceCollection AddAutoMapperService(this IServiceCollection services)
     {
         services.AddAutoMapper(cfg => cfg.AddProfile<AutoMapperProfiles>());
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuthenticationService(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!)),
+                };
+            });
 
         return services;
     }
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddDbContextServices(builder.Configuration);
 builder.Services.AddAutoMapperService();
+builder.Services.AddAuthenticationService(builder.Configuration);
 
 var app = builder.Build();
 
@@ -17,6 +18,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
