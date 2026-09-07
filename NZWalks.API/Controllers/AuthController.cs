b10cbabd7e13cd5828a197eac71abdd0c51d8057
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-07 10:55:28 +0330

    implement register

---
+++ NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO.Auths;
+
+namespace NZWalks.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AuthController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequest)
+    {
+        var identityUser = new IdentityUser()
+        {
+            UserName = registerRequest.Email,
+            Email = registerRequest.Email,
+        };
+
+        var identityResult = await _userManager.CreateAsync(identityUser, registerRequest.Password);
+
+        if (identityResult.Succeeded)
+        {
+            if (registerRequest.Roles != null && registerRequest.Roles.Any())
+            {
+                identityResult = await _userManager.AddToRolesAsync(identityUser, registerRequest.Roles);
+
+                if (identityResult.Succeeded)
+                {
+                    return Ok("You have successfully registered!");
+                }
+            }
+        }
+
+        return BadRequest("Something went wrong!");
+    }
+}
---
+++ NZWalks.API/Models/DTO/Auths/RegisterRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO.Auths;
+
+public class RegisterRequestDto
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public string[] Roles { get; set; }
+}
